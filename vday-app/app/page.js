d4fc8f19732217e89050909f81d4f84ff4acecf2
wayne-617 <wgl91603@uga.edu>
2025-01-31 00:26:51 -0500

    set up text

---
+++ vday-app/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <main>
+        <div className="flex-container">
+          <div className="text-center main-text content">
+            <h1>Will you be my Valentine?</h1>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
