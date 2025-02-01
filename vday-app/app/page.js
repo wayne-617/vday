a0d7c1b9cfa3b977a97dafdf1879e9d9b489a133
wayne-617 <wgl91603@uga.edu>
2025-02-01 00:05:32 -0500

    added envelope

--- vday-app/app/page.js
+++ vday-app/app/page.js
@@ -5,9 +5,21 @@ export default function Home() {
     <div >
       <main>
         <div className="flex-container">
+          
+          <div className="wrapper">
+            <div className="lid one"></div>
+            <div className="lid two"></div>
+            <div className="envelope"></div>
+          
+          </div>
+          
+          {/*
           <div className="text-center main-text content">
             <h1>Will you be my Valentine?</h1>
           </div>
+
+          */}
+          
         </div>
       </main>
     </div>
