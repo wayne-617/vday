c043c5b16b3ca7775914107a52b8e7ee77c427c4
wayne-617 <wgl91603@uga.edu>
2025-02-01 18:48:28 -0500

    improved letter animation

---
+++ vday-app/app/components/gif.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Gif = ({gifUrl, altText = "GIF"}) => {
+  return (
+    <div>
+      <img src={gifUrl} alt={altText} />
+    </div>
+  )
+}
--- vday-app/app/page.js
+++ vday-app/app/page.js
@@ -1,6 +1,9 @@
-import Image from "next/image";
+import { Gif } from './components/Gif'
 
 export default function Home() {
+
+  const mochaHeart = "https://media.tenor.com/cETqhEe3wmQAAAAj/mocha-bear-mocha.gif";
+
   return (
     <div >
       <main>
@@ -10,17 +13,21 @@ export default function Home() {
             <div className="lid one"></div>
             <div className="lid two"></div>
             <div className="envelope"></div>
-            <div className="text-center content transform-style-preserve">
-              <h1 className="main-text">Will you be my Valentine?</h1>
+              <div className="text-center content transform-style-preserve">
+                <h1 className="main-text">Will you be my Valentine?</h1>
 
-              <div className="flex justify-center gap-6 mt-4">
-                <button className="bg-green-500 text-white font-bold py-2 px-6 rounded-full">
-                  Yes
-                </button>
-                <button className="bg-red-500 text-white font-bold py-2 px-6 rounded-full">
-                  No
-                </button>
-              </div>
+                <div className="flex justify-center gap-6 mt-4">
+                  <button className="bg-green-500 text-white font-bold py-2 px-6 rounded-full">
+                    Yes
+                  </button>
+                  <button className="bg-red-500 text-white font-bold py-2 px-6 rounded-full">
+                    No
+                  </button>
+                </div>
+
+                <Gif gifUrl={mochaHeart} altText="Mocha Heart GIF" />
+
+              
 
             </div>
           
