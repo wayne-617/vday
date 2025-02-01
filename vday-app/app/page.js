452a76ca39e5bfbe11d1ee305659a9ef79a4746c
wayne-617 <wgl91603@uga.edu>
2025-02-01 17:30:53 -0500

    fixed colors, added buttons/animations

--- vday-app/app/page.js
+++ vday-app/app/page.js
@@ -10,16 +10,24 @@ export default function Home() {
             <div className="lid one"></div>
             <div className="lid two"></div>
             <div className="envelope"></div>
-          
-          </div>
-          
-          {/*
-          <div className="text-center main-text content">
-            <h1>Will you be my Valentine?</h1>
-          </div>
+            <div className="text-center content transform-style-preserve">
+              <h1 className="main-text">Will you be my Valentine?</h1>
 
-          */}
+              <div className="flex justify-center gap-6 mt-4">
+                <button className="bg-green-500 text-white font-bold py-2 px-6 rounded-full">
+                  Yes
+                </button>
+                <button className="bg-red-500 text-white font-bold py-2 px-6 rounded-full">
+                  No
+                </button>
+              </div>
+
+            </div>
           
+          </div>
+          
+          
+            
         </div>
       </main>
     </div>
