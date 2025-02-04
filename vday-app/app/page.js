fff2540444c12cd789da761f21c0f5226935e5c5
wayne-617 <wgl91603@uga.edu>
2025-02-03 20:59:43 -0500

    reset message and background when closed

--- vday-app/app/page.js
+++ vday-app/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Gif } from './components/Gif'
 import { Yes } from './components/Yes'
 import { Message } from './components/Message'
@@ -17,6 +17,7 @@ export default function Home() {
   const[content, setContent] = useState(<Message onYes={handleYes} onNo={handleNo}/>);
   const [background, setBackground] = useState('');
 
+
   function handleYes() {
     setContent(<Gif gifUrl={mochaHug} altText='Yay' />);
     setBackground('url("https://media.tenor.com/tKCqj_okD1UAAAAi/transparent-hearts.gif")')
@@ -30,18 +31,32 @@ export default function Home() {
     setYes(!isYes);
   };
 
+  const handleMouseLeave = () => {
+      document.body.style.pointerEvents = 'none';
+      setTimeout(() => {
+      setBackground('#ffe8e8'); // Change back after delay
+      setContent(<Message onYes={handleYes} onNo={handleNo}/>);
+      document.body.style.pointerEvents = 'auto';
+    }, 1000);
+  };
+
+  useEffect(() => {
+    document.body.style.transition = 'background 1s ease-in-out'; // Smooth transition
+  }, [background]);
+
 
 
   return (
     <div 
       style={{
       background: background,
-    }}
+      }}
+      
     >
       <main>
-        <div className="flex-container">
+        <div className="flex-container" >
           
-          <div className="wrapper">
+          <div className="wrapper" onMouseLeave={handleMouseLeave}>
           {isYes && <Yes gifUrl={mochaHug} className="yes"/>}
             <div className="lid one"></div>
             <div className="lid two"></div>
